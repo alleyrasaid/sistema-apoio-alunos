b7b1fb8a1b2878eba4eb55e7947674dcfb49c8b9
Aryella <aryella.valeska@gmail.com>
2025-09-17 18:16:30 -0300

    add React component structure, styles and gitignore

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Sistema de Apoio a Alunos</h1>
+      <p>Bem-vindo ao sistema!</p>
+    </div>
+  );
+}
+
+export default App;
